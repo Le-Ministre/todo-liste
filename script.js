dfa47d1ab7de4014e52b05fe137a5560f6be503d
Abdourahamane <abdourahamane@Abdourahamanes-iMac.local>
2024-01-05 15:16:07 +0100

    new

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let selecte1 = document.querySelector('#slecte1');
+let selecte1 = document.querySelector('#selecte1');
 let titre = document.querySelector('#titre');
 let description = document.querySelector('#description');
-let slecte2 = document.querySelector('#slecte2');
+let selecte2 = document.querySelector('#selecte2');
 let date = document.querySelector('#date');
+let statut = document.querySelector('.statut')
 let ajouter = document.querySelector('.ajouter');
+let update = document.querySelector('.update');
 let lesTr = document.querySelector('.lesTr');
-let tableau = [];
-let index = 1;
+let delit = document.querySelector('.delit');
+let edit = document.querySelector('.edit');
+let eye = document.querySelector('.eye');
+let diva = document.querySelector('.diva');
+
+let tableau = JSON.parse(localStorage.getItem('tableau')) || [];
+let indes = 1;
 let myNewChart;
 
 
@@ -18,10 +25,11 @@ stripes.forEach((stripe, intex) =>{
 })
 
 const upDateChart = () =>{
+
   let nouveau, enCours, terminer ;
-  nouveau = tableau.filter(Objet =>Objet.statut ==='Nouveau').length;
-  enCours = tableau.filter(Objet =>Objet.statut ==='En cours').length;
-  terminer = tableau.filter(Objet =>Objet.statut ==='Terminer').length;
+  nouveau = local.filter(Objet =>Objet.statut ==='Nouveau').length;
+  enCours = local.filter(Objet =>Objet.statut ==='En cours').length;
+  terminer = local.filter(Objet =>Objet.statut ==='Terminer').length;
  
 
     const ctx = document.getElementById('myChart');
@@ -47,60 +55,149 @@ const upDateChart = () =>{
 
 
 document.addEventListener('DOMContentLoaded', () =>{
-  afficheLes();
+  afficheLes()
 })
 const afficheLes = () =>{
-  lesTr.innerHTML = '';
-
-    tableau.forEach(elemt =>{
+  lesTr.innerHTML = "";
+    tableau.forEach((elemt, index) =>{
       lesTr.innerHTML += ` <tr class="stripe"> 
       <td>${elemt.id}</td>
       <td>${elemt.date}</td>
       <td>${elemt.titre}</td>
-      <td>${elemt.categorie}</td> 
-      <td> <button class="eye"> <i class="fa-solid fa-eye"  onclick"visualItem()"  style="color: #032662;"></i>  </button>
-      <button class="edit"> <i class="fa-regular fa-pen-to-square" onclick"editeItem()" style="color: #011d02;"></i> </button>                     
-      <button class="delit"> <i class="fa-solid fa-trash" onclick"deleteItem()" style="color: #9c040b;"></i></td> </button>
+      <td>${elemt.selecte1}</td> 
+      <td> <button class="eye"> <i class="fa-solid fa-eye"  onclick="visualItem()"  style="color: #032662;"></i>  </button>
+      <button class="edit" onclick="editeItem(${index})"> <i class="fa-regular fa-pen-to-square"  style="color: #011d02;"></i> </button>                     
+      <button class="delit"> <i class="fa-solid fa-trash" onclick="deleteItem(${index})" style="color: #9c040b;"></i></td> </button>
       </tr>`
     })
+
    
 } 
 
 const visualItem = () =>{
+  diva.innerHTML +=`
+  `
 
 }
 
-const editeItem = () =>{
+// edit.addEventListener('click', ()=>{
+  
+// })
+const editeItem = (index) =>{
+  update.style.display = "block"
+  ajouter.style.display = "none"
+  
 
-}
+  selecte1.value = tableau[index].selecte1;
+  titre.value = tableau[index].titre;
+  description.value = tableau[index].description;
+  date.value = tableau[index].date;
+  selecte2.value = tableau[index].statut;
 
-const deleteItem = () =>{
-
-}
 
 
-const letableau = () =>{
+ update.addEventListener('click', () =>{
+  update.style.display = "none"
+  ajouter.style.display = "block"
+
+  tableau[index].selecte1 = selecte1.value;
+  tableau[index].titre = titre.value;
+  tableau[index].description = description.value;
+  tableau[index].date = date.value;
+  tableau[index].statut = selecte2.value;
+
   let Objet = {
-    id : index,
+    id : indes,
     date : date.value,
     titre : titre.value,
-    categorie : selecte1.value,
-    statut : slecte2.value
+    selecte1 : selecte1.value,
+    statut : selecte2.value,
+    description : description.value
   }
-
   tableau.push(Objet)
-  index ++
-  localStorage.setItem('tableau', JSON.stringify(tableau));
-  tableau = JSON.parse(localStorage.getItem('tableau'));
-  console.log(lesTr);
+  indes ++
+  localStorage.setItem('local', JSON.stringify(tableau));
+  afficheLes();
+  selecte1.value ="";
+  titre.value ="";
+  description.value ="";
+  selecte2.value ="";
+  date.value ="";
+  selecte1.value ="";
+})
+ upDateChart();
+}
+function miseAjour(id) {
+ 
+  // tableau.push(Objet)
+  // let index = tableau.findIndex(rec => rec.id === id)
+  // tableau[index] = {id, date, titre, selecte1, statut, description}
 
+  // afficheLes();
+}  
+
+
+// const deleteItem = (id) =>{
+//   tableau = tableau.filter(rec => rec.id !== id);
+//   afficheLes()
+// }
+const deleteItem = (index) => {
+  // 1. Filtrer le tableau en mémoire
+
+ tableau.splice(index, 1)
+ localStorage.setItem('tableau', tableau)
+ afficheLes()
+
+  // 2. Mettre à jour le tableau en mémoire
+ 
+
+  // 3. Afficher le tableau mis à jour
+  upDateChart();
+};
+
+const letableau = () =>{
+  
+}
+function getToLocal() {
+  
 }
 
 
-ajouter.addEventListener('click', (e) =>{
-  e.preventDefault();
-  letableau();
-  afficheLes();
-  upDateChart();
+ajouter.addEventListener('click', () =>{
+  if (selecte1.value === "" ) {
+   alert('categorie is required')
+  } else if (titre.value === "") {
+    alert('titre is required')
+  } else if (description.value === "") {
+    alert('description is required')
+  } else if (date.value === "") {
+    alert('date is required')
+  } else if (selecte2.value === "") {
+    alert('selecte2 is required')
+  } else{
+
+    let Objet = {
+      id : indes ++,
+      date : date.value,
+      titre : titre.value,
+      selecte1 : selecte1.value,
+      statut : selecte2.value,
+      description : description.value
+    }
+  
+    tableau.push(Objet)
+    localStorage.setItem('tableau', JSON.stringify(tableau));
+    afficheLes();
+    upDateChart();
+   
+  }
+  selecte1.value ="";
+  titre.value ="";
+  description.value ="";
+  selecte2.value ="";
+  date.value ="";
+  selecte1.value ="";
+ 
 })
   
+
